2f38c7d37ac7f8224561e60d9e47e45cadf09445
Rajeev <develop@brandsjar.com>
2024-04-09 13:07:36 +0530

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,10 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import Nav from "@components/Nav";
 
-export const metadata = {
-  title: "Waume",
-  description: "Dog food",
-};
+
 
 const RootLayout = ({ children }) => (
   <html lang='en'>
