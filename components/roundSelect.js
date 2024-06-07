fab953579d11a9e2750b9d4ab8b2fc128df8915a
Rajeev <develop@brandsjar.com>
2024-06-07 19:59:57 +0530

    fixed countrySelect

--- components/roundSelect.js
+++ components/roundSelect.js
@@ -6,17 +6,20 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+
 const countryOptions = [
   { label: "Austria", value: "Austria", upperCost: 9.8, lowerCost: 9.8 },
   { label: "Germany", value: "Germany", upperCost: 15, lowerCost: 12.5 },
   { label: "Switzerland", value: "Switzerland", upperCost: 25, lowerCost: 20 },
 ];
+
 function getCost(value, selectedCountry) {
   let country = countryOptions.find((item) => item.value == selectedCountry);
   country = country ? country : { upperCost: 15, lowerCost: 12.5 };
   if (value.prodCost > 100) return country.lowerCost;
   else return country.upperCost;
 }
+
 function RoundSelect({
   name,
   value,
@@ -42,27 +45,29 @@ function RoundSelect({
   };
 
   return (
-    <Select onValueChange={handleChange}>
-      <SelectTrigger
-        className={`mx-4 max-w-[290px]  h-[40px]  rounded-full text-gray-500 border-[1px] border-slate-500`}
-      >
-        <SelectValue
-          className="text-center"
-          placeholder={getLabel(value[name])}
-        />
-      </SelectTrigger>
-      <SelectContent className="text-primary max-h-[200px]">
-        {options.map((option) => (
-          <SelectItem
-            key={option.value}
-            value={option.value}
-            onSelect={() => {}}
-          >
-            {option.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <div className="w-full px-4">
+      <Select onValueChange={handleChange} className="w-full px-2">
+        <SelectTrigger
+          className={`w-full h-[40px] rounded-full text-gray-500 border-[1px] border-slate-500 px-5 ${className}`}
+        >
+          <SelectValue
+            className="text-center"
+            placeholder={getLabel(value[name])}
+          />
+        </SelectTrigger>
+        <SelectContent className="text-primary z-50">
+          {options.map((option) => (
+            <SelectItem
+              key={option.value}
+              value={option.value}
+              onSelect={() => {}}
+            >
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
   );
 }
 
